ad529f6226e8b7c4ec2602d2c22ca95e3bc96e74
frederik <frederik.wiandino18@smk.belajar.id>
2025-07-19 10:04:33 +0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,4 @@
-if (typeof supabase !== 'undefined') {
-  const SUPABASE_URL = "https://zuathwjzldickgvigffd.supabase.co";
-  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1YXRod2p6bGRpY2tndmlnZmZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4ODkwNjksImV4cCI6MjA2ODQ2NTA2OX0.OjWkn6GIed1feHeoNmXZCKwD3bvOoQT7aYKQCzKJt8w";
-  window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-} else {
-  console.error("Supabase library not loaded.");
-}
+const SUPABASE_URL = "https://zuathwjzldickgvigffd.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOi..."; // kunci kamu (jangan bocorkan ke orang lain)
+
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
